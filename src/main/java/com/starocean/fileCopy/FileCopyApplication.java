64a60e7c0f764109a041fd293a75b74c637364d0
leonard <leoleonardxz@gmail.com>
2026-02-06 14:14:55 +0800

    application

---
+++ src/main/java/com/starocean/fileCopy/FileCopyApplication.java
@@ -0,0 +1,15 @@
+package com.starocean.fileCopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class FileCopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileCopyApplication.class, args);
+	}
+
+}
